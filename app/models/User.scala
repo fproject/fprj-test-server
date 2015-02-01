7a714824b6702160e1f631b4771f92b4bbe60f1e
Bui Sy Nguyen <nguyenbs@projectkit.net>
2015-02-01 19:22:35 +0700

    Update: read/write JSON date in ISO8601

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,10 +1,9 @@
 package models
 
 import org.joda.time.DateTime
-import play.api.libs.json.Json
 
 case class User(id: Option[Long], name: String, bod: DateTime)
 
 object User {
-  implicit val fmt = Json.format[User]
+
 }
